23277afa090271073e127bc8078fd0e837a7d3a9
Anton Ochel <anton.ochel1@gmail.com>
2019-04-04 15:29:47 +0200

    Junge ich kann zumindest commiten HA

---
+++ mvn/my-app/src/main/java/com/mycompany/app/BinarySearch.java
@@ -0,0 +1,10 @@
+public class BinarySearch{
+    int[] numbers;
+    public BinarySearch(int[] NumbersArray){
+        numbers = NumbersArray;
+    }
+
+    private void searchNumber(int searchedNumber){
+
+    }
+}
