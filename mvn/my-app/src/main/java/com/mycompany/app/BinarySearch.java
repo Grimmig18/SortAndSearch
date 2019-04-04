22f7555243e33d90699ebeadc350eaef2add8497
Anton Ochel <anton.ochel1@gmail.com>
2019-04-04 15:40:17 +0200

    Junge ich kann zumindest commiten HA

--- mvn/my-app/src/main/java/com/mycompany/app/BinarySearch.java
+++ mvn/my-app/src/main/java/com/mycompany/app/BinarySearch.java
@@ -1,10 +1,24 @@
 public class BinarySearch{
     int[] numbers;
-    public BinarySearch(int[] NumbersArray){
+
+    /**
+     * numbersArray must be a sorted array!
+     * @param numbersArray
+     */
+    public BinarySearch(int[] numbersArray){
         numbers = NumbersArray;
     }
 
-    private void searchNumber(int searchedNumber){
+    /**
+     * Searches for the given number in the array. If it's not inside the array, return value wil be -1.
+     * @param searchedNumber
+     * @return index of the searched number in the array
+     */
+    public int searchNumber(int searchedNumber){
+        return searchNumberRekursiv(searchedNumber, 0,numbers.length);
+    }
 
+    private int searchNumberRekursiv(int searchedNumber, int formIndex, int toIndex){
+        if()
     }
 }
