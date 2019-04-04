b9451656fbe168b9a97f17995e98da99232b88c7
NerdyStuff <NerdyStuff@users.noreply.github.com>
2019-04-04 15:48:13 +0200

    Added functional implementation of linear search

---
+++ mvn/my-app/src/main/java/com/mycompany/app/LinearSearchFunctional.java
@@ -0,0 +1,26 @@
+package com.mycompany.app;
+
+/**
+ * SortAndSearch
+ * <p>
+ * Copyright by @author Marcel Mertens
+ * Website: https://mertens-web.ddns.net
+ * <p>
+ * Date: 04.04.2019
+ */
+public class LinearSearchFunctional
+{
+
+    private int[] zSearchArray;
+
+    public LinearSearchFunctional(int[] pSearchArray)
+    {
+        zSearchArray = pSearchArray;
+    }
+
+    public int search()
+    {
+
+        return -1;
+    }
+}
