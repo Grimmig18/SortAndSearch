869fc8508201c479bce2e327820aed268a4c7722
NerdyStuff <NerdyStuff@users.noreply.github.com>
2019-04-05 10:53:41 +0200

    Added multithreaded linear search

---
+++ mvn/my-app/src/main/java/com/mycompany/app/LinearSearchMulti.java
@@ -0,0 +1,26 @@
+package com.mycompany.app;
+
+public class LinearSearchMulti
+{
+    int[] zSearchArray;
+    int zThreads = 1;
+
+    public LinearSearchMulti(int[] pSearchArray)
+    {
+        zSearchArray = pSearchArray;
+    }
+
+    public LinearSearchMulti(int[] pSearchArray, int pThreads)
+    {
+        zSearchArray = pSearchArray;
+        zThreads = pThreads;
+    }
+
+    public int search(int pSearchElement)
+    {
+
+
+
+        return -1;
+    }
+}
