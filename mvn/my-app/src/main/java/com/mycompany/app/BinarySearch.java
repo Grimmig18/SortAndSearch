398373f619b4c8266f6797f0e07ac0753258747e
Anton Ochel <anton.ochel1@gmail.com>
2019-04-08 12:40:08 +0200

    BRAAHHH binarysearch funktionier

--- mvn/my-app/src/main/java/com/mycompany/app/BinarySearch.java
+++ mvn/my-app/src/main/java/com/mycompany/app/BinarySearch.java
@@ -1,3 +1,4 @@
+package com.mycompany.app;
 public class BinarySearch{
     int[] numbers;
 
@@ -18,13 +19,16 @@ public class BinarySearch{
         return searchNumberRekursiv(searchedNumber, 0,numbers.length);
     }
     private int searchNumberRekursiv(int searchedNumber, int fromIndex, int toIndex){
-        int middle = ((int)(fromIndex - toIndex)/2) + fromIndex;
+        int middle = ((toIndex- fromIndex)/2) + fromIndex;
+        if(fromIndex > toIndex){
+            return -1;
+        }
         if(numbers[middle]>searchedNumber){
             return searchNumberRekursiv(searchedNumber,fromIndex,middle-1);
         }else if(numbers[middle]<searchedNumber){
             return searchNumberRekursiv(searchedNumber,middle+1,toIndex);
         }else if(numbers[middle]==searchedNumber)return middle;
-        else return -1;
+        else return -2;
     }
 
 }
--- mvn/my-app/src/main/java/com/mycompany/app/BinarySearchTest.java
+++ mvn/my-app/src/main/java/com/mycompany/app/BinarySearchTest.java
@@ -1,11 +1,13 @@
+package com.mycompany.app;
 public class BinarySearchTest{
 
     public static void main(String[] args) {
-        int[] array = new int[100];
-        for (int i = 0; i<100;i++){
-            array[i] = i*2;
+        int[] array = new int[100000000];
+        for (int i = 0; i<array.length;i++){
+            array[i] = i;
         }
 
         BinarySearch bs = new BinarySearch(array);
+        System.out.println(bs.searchNumber(12348578));
     }
 }
