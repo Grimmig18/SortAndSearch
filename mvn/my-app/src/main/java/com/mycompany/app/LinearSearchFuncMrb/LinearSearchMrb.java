30cbcbc8a0547fa969fd50f907b6f513bcffaadc
TristanMrb <trisemig@aol.com>
2019-04-12 19:27:47 +0200

    Add LinearSearchFuncMrb Package

---
+++ mvn/my-app/src/main/java/com/mycompany/app/LinearSearchFuncMrb/LinearSearchMrb.java
@@ -0,0 +1,9 @@
+package com.mycompany.app.LinearSearchFuncMrb;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface LinearSearchMrb {
+        public List search(int [] arr, int n);
+}
+
