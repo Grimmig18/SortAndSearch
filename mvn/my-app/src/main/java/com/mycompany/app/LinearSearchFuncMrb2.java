9ccbe07efa57f7d8078c103886c4a51e5dbc6543
TristanMrb <trisemig@aol.com>
2019-04-12 09:28:12 +0200

    Update LinearSearchFuncMrb2

--- mvn/my-app/src/main/java/com/mycompany/app/LinearSearchFuncMrb2.java
+++ mvn/my-app/src/main/java/com/mycompany/app/LinearSearchFuncMrb2.java
@@ -7,30 +7,30 @@ import java.util.stream.IntStream;
 public class LinearSearchFuncMrb2 {
 
     interface LinearSearch {
-        List Search(int n);
+        List Search(int [] arr, int n);
     }
 
-    public static void SearchInt(LinearSearch search, int search1){
-        search.Search(search1);
+    public static void SearchInt(LinearSearch search, int [] arr, int search1){
+        search.Search(arr, search1);
     }
 
     public static void main(String[] args) {
 
-        int[] zahlen = {0,1,2,1,4,1,6,1,2,1};
+        //int[] zahlen = {0,1,2,1,4,1,6,1,2,1};
 
-        //int [] zahlen = new int [10];
+        int [] zahlen = new int [50];
 
-        //for (int i=0; i<10; i++)
-        //  zahlen[i] = (int) (Math.random() *10);
+        for (int i=0; i<50; i++)
+          zahlen[i] = (int) (Math.random() *50);
 
         for (int i : zahlen) {
             System.out.print(i + ",");
         }
 
-        LinearSearch search = (str) -> {
+        LinearSearch search = (arr, str) -> {
             List list = IntStream
-                    .range(0, zahlen.length-1)
-                    .filter(i -> zahlen[i] == str)
+                    .range(0, arr.length)
+                    .filter(i -> arr[i] == str)
                     .mapToObj(i -> i)
                     .collect(Collectors.toList());
 
@@ -40,7 +40,7 @@ public class LinearSearchFuncMrb2 {
             return list;
         };
 
-        SearchInt(search, 1);
+        SearchInt(search, zahlen,1);
     }
 
 }
